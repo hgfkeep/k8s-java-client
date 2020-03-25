0a08adefc8066019b8471f1bf870671da18fe218
hgfkeep <hgfkeep@gmail.com>
2020-03-25 21:06:47 +0800

    add: jdkdoc

---
+++ src/test/java/io/kubernetes/client/ApiClientTest.java
@@ -0,0 +1,13 @@
+package io.kubernetes.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApiClientTest {
+
+    @Test
+    public void getBasePath() {
+        System.out.println("Welcome to use!");
+    }
+}
